c6c2a53b956956c7c321a26e9828f98763e1551a
Abir Hasan <cse.170201063@gmail.com>
2022-10-08 02:02:31 +0600

    53-8 Remove Item handler and Make Data consistent across UI

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -2,8 +2,8 @@ import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import {  FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css'
-const ReviewItem = ({ product }) => {
-    const {name,price,quantity,img,shipping}=product;
+const ReviewItem = ({ product,handleRemoveItem }) => {
+    const {id,name,price,quantity,img,shipping}=product;
     return (
       <div className="review-item">
         <div>
@@ -17,7 +17,7 @@ const ReviewItem = ({ product }) => {
                     <p> Quantity: { quantity}</p>
                 </div>
                 <div className="delete-container">
-                    <button className='delete-button'>
+                    <button onClick={()=>handleRemoveItem(id)} className='delete-button'>
                         <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
                     </button>
                 </div>
